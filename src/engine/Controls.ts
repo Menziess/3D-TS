5e018bd83a58927a2fdeb67a7d53d4c480c1bb01
STEFAN-DESKTOP\Stefan <stefan_schenk@hotmail.com>
2017-03-15 23:35:58 +0100

    Rearranged up and down movement, fixed mouse event

--- src/engine/Controls.ts
+++ src/engine/Controls.ts
@@ -54,7 +54,7 @@ export default class FirstPersonControls {
   /**
    * Initialization
    */
-  private init = function() {
+  private init = function () {
     if (this.canvas !== document) this.canvas.setAttribute('tabindex', -1);
     this.canvas.addEventListener('contextmenu', function (event) { event.preventDefault(); }, false);
     this.canvas.addEventListener('mousemove', this.bind(this, this.onMouseMove), false);
@@ -81,6 +81,8 @@ export default class FirstPersonControls {
    */
   private onMouseUp = function (event) {
     this.activeLook = false;
+    this.mouseX = 0;
+    this.mouseY = 0;
   };
 
 
@@ -157,17 +159,13 @@ export default class FirstPersonControls {
    */
   private onKeyDown = function (event) {
     switch (event.keyCode) {
-      case 16: /*shift*/ this.movementSpeed = 2.0; break;
-      case 38: /*up*/
-      case 87: /*W*/ this.moveForward = true; break;
-      case 37: /*left*/
-      case 65: /*A*/ this.moveLeft = true; break;
-      case 40: /*down*/
-      case 83: /*S*/ this.moveBackward = true; break;
-      case 39: /*right*/
-      case 68: /*D*/ this.moveRight = true; break;
-      case 82: /*R*/ this.moveUp = true; break;
-      case 70: /*F*/ this.moveDown = true; break;
+      case 16: /*shift*/  this.movementSpeed = 2.0; break;
+      case 87: /*W*/      this.moveForward = true; break;
+      case 65: /*A*/      this.moveLeft = true; break;
+      case 83: /*S*/      this.moveBackward = true; break;
+      case 68: /*D*/      this.moveRight = true; break;
+      case 32: /*space*/  this.moveUp = true; break;
+      case 88: /*X*/      this.moveDown = true; break;
     }
   };
 
@@ -177,17 +175,13 @@ export default class FirstPersonControls {
    */
   private onKeyUp = function (event) {
     switch (event.keyCode) {
-      case 16: /*shift*/ this.movementSpeed = 1.0; break;
-      case 38: /*up*/
-      case 87: /*W*/ this.moveForward = false; break;
-      case 37: /*left*/
-      case 65: /*A*/ this.moveLeft = false; break;
-      case 40: /*down*/
-      case 83: /*S*/ this.moveBackward = false; break;
-      case 39: /*right*/
-      case 68: /*D*/ this.moveRight = false; break;
-      case 82: /*R*/ this.moveUp = false; break;
-      case 70: /*F*/ this.moveDown = false; break;
+      case 16: /*shift*/  this.movementSpeed = 1.0; break;
+      case 87: /*W*/      this.moveForward = false; break;
+      case 65: /*A*/      this.moveLeft = false; break;
+      case 83: /*S*/      this.moveBackward = false; break;
+      case 68: /*D*/      this.moveRight = false; break;
+      case 32: /*space*/  this.moveUp = false; break;
+      case 88: /*X*/      this.moveDown = false; break;
     }
   };
 
